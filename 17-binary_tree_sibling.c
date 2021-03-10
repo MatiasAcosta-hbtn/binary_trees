df34ff67bcaa334aac720121f948ebf9d6e8c98d
Matias Acosta <2277@holbertonschool.com>
2021-03-10 12:00:24 -0300

    Fix betty task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-    
-    if (node->parent->left != node)
-        return (node->parent->left);
-    else
-        return (node->parent->right);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left != node)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
